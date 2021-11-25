42e08bdfa0f406abc51ccfafc112fe10b14ea45e
DoksaVPC <55451950+DoksaVPC@users.noreply.github.com>
2021-11-25 22:02:02 +0100

    ios ui fix

--- Assets/Scripts/UI/PinToSafeArea.cs
+++ Assets/Scripts/UI/PinToSafeArea.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PinToSafeArea : MonoBehaviour
 {
     private Rect lastSafeArea;
     [SerializeField]
     private RectTransform parentRectTransform;
+    [SerializeField]
+    private bool isSelectionScreen;
+    [SerializeField]
+    private VerticalLayoutGroup verticalLayoutGroup;
+    private const int defaultPadding = -600;
 
     private void Update()
     {
@@ -19,17 +25,22 @@ public class PinToSafeArea : MonoBehaviour
     private void ApplySafeArea()
     {
         Rect safeAreaRect = Screen.safeArea;
-
         float scaleRatio = parentRectTransform.rect.width / Screen.width;
 
-        var left = safeAreaRect.xMin * scaleRatio;
-        var right = -(Screen.width - safeAreaRect.xMax) * scaleRatio;
-        var top = -safeAreaRect.yMin * scaleRatio;
-        var bottom = (Screen.height - safeAreaRect.yMax) * scaleRatio;
+        if (!isSelectionScreen)
+        {
+            var left = 0;
+            var right = 0;
+            var top = -safeAreaRect.yMin * scaleRatio;
+            var bottom = 0;
 
-        RectTransform rectTransform = GetComponent<RectTransform>();
-        rectTransform.offsetMin = new Vector2(left, bottom);
-        rectTransform.offsetMax = new Vector2(right, top);
+            RectTransform rectTransform = GetComponent<RectTransform>();
+            rectTransform.offsetMin = new Vector2(left, bottom);
+            rectTransform.offsetMax = new Vector2(right, top);
+        } else
+        {
+            verticalLayoutGroup.padding.top = defaultPadding + (int)(safeAreaRect.yMin * scaleRatio);
+        }   
 
         lastSafeArea = Screen.safeArea;
     }
